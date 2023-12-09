4903ab9599db886b1e5af32ef4a6f4536bfc215b
Peter Didow <petermartind@outlook.com>
2023-12-09 10:09:58 -0700

    Add files via upload

---
+++ mspacAni.py
@@ -0,0 +1,196 @@
+#!/usr/bin/env python
+# ---------------------------------------------------------
+# Filename: devilDance.py
+# ---------------------------------------------------------
+# Demonstration of the features in the multilineMAX7219 library
+#
+# v1.0
+# F.Stern 2014
+# ---------------------------------------------------------
+# improved and extended version of JonA1961's MAX7219array
+# ( https://github.com/JonA1961/MAX7219array )
+# ---------------------------------------------------------
+# See multilineMAX7219.py library file for more details
+# ---------------------------------------------------------
+# This demo script is intended to run on an array of 9 (3x3)
+#   MAX7219 boards, connected as described in the library
+#   script.
+# The variables MATRIX_WIDTH and MATRIX_HEIGHT, defined in the
+#   multilineMAX7219.py library script, should always be set to be
+#   consistent with the actual hardware setup in use.  If it is
+#   not set correctly, then the functions will not work as
+#   intended
+# ---------------------------------------------------------
+
+import time
+from random import randrange
+import numpy as np
+# Import library
+import multilineMAX7219 as LEDMatrix
+# Import fonts
+from multilineMAX7219_fonts import CP437_FONT, SINCLAIRS_FONT, LCD_FONT, TINY_FONT
+
+# The following imported variables make it easier to feed parameters to the library functions
+from multilineMAX7219 import DIR_L, DIR_R, DIR_U, DIR_D
+from multilineMAX7219 import DIR_LU, DIR_RU, DIR_LD, DIR_RD
+from multilineMAX7219 import DISSOLVE, GFX_ON, GFX_OFF, GFX_INVERT
+# Import  matrix Rotation
+from rotate_matrix import rotate_matrix
+# Initialise the library and the MAX7219/8x8LED arrays
+LEDMatrix.init()
+
+try:
+    # Define mspac image1 as a pixel map
+    mspac103 = [
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0],
+        [0, 0, 0, 1, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0],
+        [0, 1, 1, 0, 1, 1, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0],
+        [0, 1, 1, 1, 1, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 1, 1, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 1, 1, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    ]
+    # Define mspac image1 as a pixel map
+    mspac104 = [
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0],
+        [0, 0, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0],
+        [0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0],
+        [0, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 0, 0],
+        [0, 0, 1, 1, 1, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0],
+        [0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 1, 1, 1, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0],
+        [0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0],
+        [0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    ]
+    # Define mspac image1 as a pixel map
+    mspac105 = [
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0],
+        [0, 0, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0],
+        [0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0],
+        [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0],
+        [0, 0, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0],
+        [0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [0, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+        [0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0],
+        [0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0],
+        [0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    ]
+    # Define mspac image1 as a pixel map
+    mspac106 = [
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0],
+        [0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    ]
+    # Define mspac image1 as a pixel map
+    mspac107 = [
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0],
+        [0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0],
+        [0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0],
+        [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0],
+        [0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0],
+        [0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0],
+        [0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0],
+        [0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+
+    ]
+
+    # Flip the frames vertically
+
+    mspac107 = rotate_matrix(mspac107, 180)
+    mspac106 = rotate_matrix(mspac106, 180)
+    mspac105 = rotate_matrix(mspac105, 180)
+    mspac104 = rotate_matrix(mspac104, 180)
+    mspac103 = rotate_matrix(mspac103, 180)
+
+    # Initialize x, direction, and flip_horizontal
+    x = 0
+    direction = 1  # Initial direction is forward (1)
+    flip_horizontal = False
+
+    # Display Pacman animation forever
+    while True:
+        #LEDMatrix.gfx_set_all(GFX_OFF)
+        # Update x based on direction
+        x += direction
+
+        # Check if x reaches the boundaries and reverse direction
+        if x == 32:
+            direction = -1
+            flip_horizontal = True
+        elif x == 0:
+            direction = 1
+            flip_horizontal = False
+
+        # Apply horizontal flip and display
+        LEDMatrix.gfx_set_all(GFX_OFF)
+ 
+        #frame_to_display = np.fliplr(
+        #mspac103) if flip_horizontal else mspac103
+        frame_to_display = mspac103 if flip_horizontal else np.fliplr(mspac103)
+        LEDMatrix.gfx_sprite_array(frame_to_display, 0, x)
+        LEDMatrix.gfx_render()
+        time.sleep(0.05)
+        LEDMatrix.gfx_set_all(GFX_OFF)
+
+        #frame_to_display = np.fliplr(mspac104) if flip_horizontal else mspac104
+        frame_to_display = mspac104 if flip_horizontal else np.fliplr(mspac104)
+        LEDMatrix.gfx_sprite_array(frame_to_display, 0, x)
+        LEDMatrix.gfx_render()
+        time.sleep(0.05)
+        LEDMatrix.gfx_set_all(GFX_OFF)
+
+        #frame_to_display = np.fliplr(
+        #mspac105) if flip_horizontal else mspac105
+        frame_to_display = mspac105 if flip_horizontal else np.fliplr(mspac105)
+        LEDMatrix.gfx_sprite_array(frame_to_display, 0, x)
+        LEDMatrix.gfx_render()
+        time.sleep(0.05)
+
+
+except KeyboardInterrupt:
+    # reset array
+    LEDMatrix.scroll_message_horiz(["", "Goodbye!", ""], 1, 8)
+    LEDMatrix.clear_all()
