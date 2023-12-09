11c852a81185ebc996fb39ef96e2a66c2fc1a39c
Peter Didow <petermartind@outlook.com>
2023-12-09 10:11:39 -0700

    Update mspacAni.py

--- mspacAni.py
+++ mspacAni.py
@@ -1,25 +1,11 @@
 #!/usr/bin/env python
 # ---------------------------------------------------------
-# Filename: devilDance.py
+# Filename: mspacAni.py
+# Peter Didow 2023
 # ---------------------------------------------------------
 # Demonstration of the features in the multilineMAX7219 library
 #
-# v1.0
-# F.Stern 2014
-# ---------------------------------------------------------
-# improved and extended version of JonA1961's MAX7219array
-# ( https://github.com/JonA1961/MAX7219array )
-# ---------------------------------------------------------
-# See multilineMAX7219.py library file for more details
-# ---------------------------------------------------------
-# This demo script is intended to run on an array of 9 (3x3)
-#   MAX7219 boards, connected as described in the library
-#   script.
-# The variables MATRIX_WIDTH and MATRIX_HEIGHT, defined in the
-#   multilineMAX7219.py library script, should always be set to be
-#   consistent with the actual hardware setup in use.  If it is
-#   not set correctly, then the functions will not work as
-#   intended
+# 
 # ---------------------------------------------------------
 
 import time
